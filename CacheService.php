a254c64cb8776593902015dc6331ee2b04494303
Shivam Maharshi <shivam.maharshi@gmail.com>
2016-08-30 20:12:00 -0400

    Added persistence to cacheService.php
    1. Added persistence to cacheService.
    2. Use MD5 for hashing URL to key mapping.
    3. Updated functionality to only add response header & response body to cache & cache file.

--- CacheService.php
+++ CacheService.php
@@ -1,63 +1,95 @@
 <?php
 
 error_reporting(E_ALL);
-include 'MemcacheClient.php';
+#include 'MemcacheClient.php';
+include 'RedisClient.php';
 #include '/var/www/html/apache-log4php-2.3.0/src/main/php/Logger.php';
 
-define('MEMCACHED_HOST', '127.0.0.1');
-define('MEMCACHED_PORT', '11211');
+define('HOST', '127.0.0.1');
+#define('PORT', '11211');
+define('PORT', '6379');
+define('ARCHIVE_DIR', '/var/www/html/archive/');
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 $fullURLPath = $_SERVER['REQUEST_URI'];
 $segments = explode('http://', $_SERVER['REQUEST_URI']);
 $cacheURL = "http://".$segments[1];
+$cacheURL = md5($cacheURL);
 
 Logger::configure('config.xml');
 $logger = Logger::getLogger("myLogger");
 
-// Create connection
-$memcacheClient = new MemcacheClient(MEMCACHED_HOST, MEMCACHED_PORT);
-$cacheAvailable = $memcacheClient->connect();
+$cache = new RedisClient(HOST, PORT);
+$cacheAvailable = $cache->connect();
 
 if($cacheAvailable) {
-  $logger->info("Successfully connected to Memcache server: ".MEMCACHED_HOST.":".MEMCACHED_PORT);
+  $logger->info("Successfully connected to cache server: ".HOST.":".PORT);
 } else {
-  $logger->error("Can't connect to Memcache. Make sure it is up and running!");
+  $logger->error("Can't connect to cache. Make sure it is up and running!");
 }
 
-$logger->info("Method: ".$httpMethod);
+$logger->debug("Method: ".$httpMethod);
 
 if($httpMethod == 'PUT') {
     $logger->info("Writing cached response for URL: ".$cacheURL);
     $handle = fopen('php://input', 'rb');
     $rawPutData = '';
-    while($chunk = fread($handle, 1024)) {
-      $rawPutData .= $chunk;
+    $is_response = false;
+    if($handle) {
+      while(($buffer = fgets($handle, 4096)) !== false) {
+        if($is_response) {
+          $rawPutData .= $buffer;
+        } else {
+          $buffer = trim($buffer);
+          if($buffer == '') {
+            $is_response = true;
+          }
+        }
+      }
+      if(!feof($handle)) {
+        $logger->error("Error: unexpected fgets() fail.");
+      }
     }
-   if($cacheAvailable) {
-    $logger->info("Writing PUT request body to Memcache: ".$rawPutData);
-    $newKeyAdded = $memcacheClient->add($cacheURL, $rawPutData);
+
+  if($cacheAvailable) {
+    $logger->debug("Writing PUT request body to cache: ".$rawPutData);
+    $newKeyAdded = $cache->add($cacheURL, $rawPutData);
+
     if($newKeyAdded) {
-      $logger->error("URL adding success: ".$cacheURL);    
+      $logger->debug("URL adding success: ".$cacheURL);    
     } else {
-      $logger->error("URL already exists: ".$cacheURL);
+      $logger->info("URL already exists: ".$cacheURL);
     }
-   } else {
-    $logger->error("Can't connect to Memcache. Make sure it is up and running!");
-   }
-} elseif($httpMethod == 'GET') {
-    $logger->info("Fetching cached response for URL from Memcache: ".$cacheURL);
-    if($cacheAvailable) {
-      $logger->error("Response for URL: ".$memcacheClient->get($cacheURL));
-      print_r("Response for URL: ".$memcacheClient->get($cacheURL));
+  } else {
+    $logger->error("Can't connect to cache. Make sure it is up and running!");
+  }
+
+  # Write response to a file.
+  $file = ARCHIVE_DIR.$cacheURL.".txt";
+  if(file_exists($file)) {
+    $logger->info("Response cache file already exists! : ".$file);
+  } else {
+    $logger->info("Writing file: ".$file);
+    $fileWriteStatus = file_put_contents($file, $rawPutData);
+    if($fileWriteStatus) {
+      $logger->info("File written successfully: ".$file);
     } else {
-      $logger->error("Can't connect to Memcache. Make sure it is up and running!");
+      $logger->error("File writting failed: ".$file);
+    }
+  }
+} elseif($httpMethod == 'GET') {
+    $logger->info("Fetching cached response for URL from cache: ".$cacheURL);
+    if($cacheAvailable) {
+      $logger->debug("Response for URL: ".$cache->get($cacheURL));
+      print_r($cache->get($cacheURL));
+    } else {
+      $logger->error("Can't connect to cache. Make sure it is up and running!");
     }
 } else {
     $logger->error("Unsupported HTTP Method: ".$httpMethod);
 }
 
-$memcacheClient->close();
+$cache->close();
 $logger->info("Closed client connection");
-unset($fullURLPath, $segments, $cacheURL, $cacheAvailable, $httpMethod, $memcacheClient);
+unset($fullURLPath, $segments, $cacheURL, $cacheAvailable, $httpMethod, $cache);
 ?>
