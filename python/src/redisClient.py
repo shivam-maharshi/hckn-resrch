2f591552e12595af56bc48a31f2bc6f80c529314
shivam-maharshi <shivam.maharshi@gmail.com>
2016-11-05 23:52:44 -0400

    Implemented caching service.

---
+++ python/src/redisClient.py
@@ -0,0 +1,18 @@
+import redis;
+
+'''
+Thin RedisClient for operations on Redis.
+
+@author: shivam.maharshi
+'''
+class RedisClient(object):
+
+    rc = redis.StrictRedis(host='127.0.0.1', port=6379, db=0);
+
+    @staticmethod
+    def set(key, data):
+        return RedisClient.rc.set(key, data)
+
+    @staticmethod
+    def get(key):
+        return RedisClient.rc.get(key)
--- python/src/views.py
+++ python/src/views.py
@@ -1,12 +1,49 @@
+import os
 from src import app
+from src.redisClient import RedisClient
+
 from flask.templating import render_template
+from flask import request
+
+'''
+View for all service end points.
+
+@author: shivam.maharshi
+'''
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', message='The caching service is up!')
+    return render_template('index.html', message='The caching service is up and running!')
 
 @app.route('/cacheService/<path:pageUrl>', methods=['PUT'])
-def cache(pageUrl):
-    
-    return "Successfully saved!", 200;
+def cache(pageUrl=None):
+    try:
+        lineSep = os.linesep
+        data = ''.join(request.data.decode('utf-8')).split(lineSep + lineSep)
+        lastModified = fetchHeaderValue(data[0], "Last-Modified")
+        pageContent = ''
+        i = 1;
+        while i < len(data):
+            pageContent += data[i] + '\n'
+            i = i+1
+
+        RedisClient.set(pageUrl.strip() + lastModified, pageContent)
+        return "Successfully saved!", 200;
+    except Exception:
+        return "Request unsuccessful!", 500;
+
+def fetchHeaderValue(data, header) :
+    res = ''
+    data = data.split('\n')
+    readingValue = False
+    for d in data:
+        if (d.startswith(header)):
+            for c in d:
+                if (not readingValue and c == ':'):
+                    readingValue = True
+                elif (readingValue):
+                    res += c
+        else:
+            continue
+    return res.strip()
