a2b26c70372c85bc95e44142e071993d596feeb9
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-01-30 02:22:46 -0500

    Update MySQLClient.php

--- cache-ws/php/MySQLClient.php
+++ cache-ws/php/MySQLClient.php
@@ -1,30 +1,18 @@
 <?php
-
 error_reporting(E_ALL);
-
 $title = $_GET["title"];
-
-print_r($title);
-
 $conn = new mysqli("127.0.0.1","root","root","wiki");
 if($conn->connect_error) {
-	die("Connection failed ".mysqli_connect_error());
+  die("Connection failed ".mysqli_connect_error());
 }
-
-$sql = "select page_latest from page where page_title='".$title."' and page_namespace=0";
-$result = $conn->query($sql);
-$row = $result->fetch_assoc();
-$oldId = $row["page_latest"];
-print_r($oldId);
-
-$sql1 = "select old_text from text where old_id = 5649748";
+$sql0 = "select page_latest from page where page_title='".$title."' and page_namespace=0";
+$result0 = $conn->query($sql0);
+$row0 = $result0->fetch_assoc();
+$oldId = $row0["page_latest"];
+$sql1 = "select old_text from text where old_id = ".$oldId;
 $result1 = $conn->query($sql1);
-print_r($result1);
 $row1 = $result1->fetch_assoc();
-print_r($row1);
 $data = $row1["old_text"];
 print_r("Data : ".$data);
-
 $conn->close();
-
 ?>
